c377992b9d290cf7ff2dc4ae45b4066ed8e3dea8
Emrace <acesinexile@gmail.com>
2013-07-27 14:22:16 +0300

    Added Methods HW - *not finished*

---
+++ 09.Methods/07.ReverseNumbers/ReverseNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+class ReverseNumbers
+{
+    static decimal ReverseNumberOrder(decimal number)
+    {
+        string digitCount = number.ToString();
+        string digitsAfterSeparator = "";
+        if (number != (int)number)
+        {
+            digitsAfterSeparator = digitCount.Substring(digitCount.IndexOf('.') + 1);
+            digitCount = digitCount.Remove(digitCount.IndexOf('.'));
+        }
+        string numberWithoutSeparator = digitCount + digitsAfterSeparator;
+        int numberAsInt = int.Parse(numberWithoutSeparator);
+        StringBuilder sb = new StringBuilder();
+        int remainder = 0;
+        for (int i = 0; i < numberWithoutSeparator.Length; i++)
+        {
+            remainder = numberAsInt % 10;
+            numberAsInt /= 10;
+            if (remainder > 9)
+            {
+                ReverseNumberOrder(number);
+
+            }
+            sb.Append(remainder);
+        }
+        decimal reversedIntNumber = int.Parse(sb.ToString());
+        decimal divider = (decimal)Math.Pow(10, digitCount.Length);
+        decimal reversedNumber = reversedIntNumber / divider;
+        return reversedNumber;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter a number: ");
+        decimal number = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("The reversed number is: {0}.", ReverseNumberOrder(number));
+    }
+}
---
+++ 09.Methods/AppearanceCountTest/AppearanceCountTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppearanceCountTest
+{
+    [TestClass]
+    public class AppearanceCountTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           int[] array = { 24, 23, 7, 24, 117, 75, 24, 13, 15, };
+           int count = NumberOfAppearances.AppearanceCount(array, 24);
+           Assert.AreEqual(3, count);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] array = { 24, 23, 75, 43, 9, 0, 75, 15, 757, 19, 75, 24, 117, 75, 24, 13, 15, };
+            int count = NumberOfAppearances.AppearanceCount(array, 75);
+            Assert.AreEqual(4, count);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] array = { 24, 85, 13, 34, 23, 7, 24, 117, 75, 24, 13, 15, };
+            int count = NumberOfAppearances.AppearanceCount(array, 7);
+            Assert.AreEqual(1, count);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] array = { 24, 23, 7, 24, 117, 75, 24, 13, 15, };
+            int count = NumberOfAppearances.AppearanceCount(array, 9);
+            Assert.AreEqual(0, count);
+        }
+    }
+}
